cbbc043639b3ec7cd11dfcbf095a176c7cc31c8e
lavegaa <cksgh405@naver.com>
2021-07-05 22:21:44 +0900

    server test

---
+++ server/README.md
@@ -0,0 +1,3 @@
+# i18n-json-generator
+
+fist commit
---
+++ server/src/app.ts
@@ -0,0 +1,21 @@
+import fastify, { FastifyInstance } from 'fastify'
+import { Server, IncomingMessage, ServerResponse } from 'http'
+import dbConnection from './plugins/dbConnection'
+import routes from './routes/routes'
+const server: FastifyInstance<Server, IncomingMessage, ServerResponse> =
+  fastify()
+
+server.register(dbConnection)
+server.register(routes)
+server.register(require('fastify-cors'), {
+  // put your options here
+})
+
+server.get('/', async (request, reply) => {
+  return { hello: 'world hello' }
+})
+
+server.listen(3001, '0.0.0.0', function (err) {
+  if (err) throw err
+  server.log.info(`server listening on`)
+})
