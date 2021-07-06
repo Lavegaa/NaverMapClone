2594af62c70caae7d8a81e9ad9c542b8d4ab11ab
lavegaa <cksgh405@naver.com>
2021-07-06 21:24:01 +0900

    remove swagger

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,30 +9,6 @@ const server: FastifyInstance<Server, IncomingMessage, ServerResponse> =
 server.register(dbConnection)
 server.register(favorite)
 
-server.register(require('fastify-swagger'), {
-  exposeRoute: true,
-  routePrefix: '/documentation',
-  swagger: {
-    info: {
-      title: 'GATEWAY SERVICE',
-      description: 'All micro-services',
-      version: '1.0.0',
-    },
-    host: '3.16.131.32:3001',
-    schemes: '',
-    consumes: ['application/json'],
-    produces: ['application/json'],
-    tags: [{ name: 'Auth', description: 'Auth related endpoints' }],
-    securityDefinitions: {
-      Authorization: {
-        type: 'apiKey',
-        name: 'Authorization',
-        in: 'header',
-      },
-    },
-  },
-})
-
 server.register(require('fastify-cors'), {
   // put your options here
 })
