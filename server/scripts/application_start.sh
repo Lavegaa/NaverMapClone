113b3026f54e313fbf4a89eb2cff002a8576269b
lavegaa <cksgh405@naver.com>
2021-07-15 00:17:23 +0900

    수정4

--- server/scripts/application_start.sh
+++ server/scripts/application_start.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+[ -s "/home/ubuntu/.nvm/nvm.sh" ] && \. "/home/ubuntu/.nvm//nvm.sh"  # This loads nvm
+[ -s "/home/ubuntu/.nvm//bash_completion" ] && \. "/home/ubuntu/.nvm//bash_completion"
 cd /home/ubuntu/server
 
 npm run start
