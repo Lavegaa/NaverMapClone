f43f3cc5349044fee0263e284d5bbc5d8fd63b32
lavegaa <cksgh405@naver.com>
2021-07-14 23:57:05 +0900

    nvm설정넣기

--- server/scripts/before_install.sh
+++ server/scripts/before_install.sh
@@ -3,12 +3,9 @@
 
 
 # Install node.js
-sudo apt-get install python-software-properties -y
-sudo apt-add-repository ppa:chris-lea/node.js -y
-sudo apt-get update
-sudo apt-get install nodejs -y
-source ~/.bashrc 
-
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 # Install nodemon
 # sudo npm install nodemon -g
 
