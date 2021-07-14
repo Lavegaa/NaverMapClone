597a2051a3c0f645a58e045872d9fecfcb5c2992
lavegaa <cksgh405@naver.com>
2021-07-15 00:22:50 +0900

    수정5

--- server/scripts/after_install.sh
+++ server/scripts/after_install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-[ -s "/home/ubuntu/.nvm/nvm.sh" ] && \. "/home/ubuntu/.nvm//nvm.sh"  # This loads nvm
-[ -s "/home/ubuntu/.nvm//bash_completion" ] && \. "/home/ubuntu/.nvm//bash_completion"
 cd /home/ubuntu/server
 
 sudo npm install pm2 -g
--- server/scripts/application_start.sh
+++ server/scripts/application_start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-[ -s "/home/ubuntu/.nvm/nvm.sh" ] && \. "/home/ubuntu/.nvm//nvm.sh"  # This loads nvm
-[ -s "/home/ubuntu/.nvm//bash_completion" ] && \. "/home/ubuntu/.nvm//bash_completion"
+
 cd /home/ubuntu/server
 
 npm run start
