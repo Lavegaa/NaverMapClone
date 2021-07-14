fd1cd2bd49d786738aaa473b46fff509e54e59ba
lavegaa <cksgh405@naver.com>
2021-07-15 00:12:10 +0900

    수정2

--- server/scripts/before_install.sh
+++ server/scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install node.js
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="~/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 
