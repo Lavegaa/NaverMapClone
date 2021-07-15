ef7ef8b00bd694589c60a2e98493bb302f270fe8
lavegaa <cksgh405@naver.com>
2021-07-15 22:55:22 +0900

    cd추가

--- server/scripts/after_install.sh
+++ server/scripts/after_install.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# cd /home/ubuntu/server
-# pm2
-# npm install pm2 -g
-export NVM_DIR="~/.nvm"
-[ -s "/home/ubuntu/.nvm/nvm.sh" ] && \. "/home/ubuntu/.nvm/nvm.sh"  # This loads nvm
-[ -s "/home/ubuntu/.nvm/bash_completion" ] && \. "/home/ubuntu/.nvm/bash_completion"
+cd /home/ubuntu/server
+sudo npm install
 sudo npm install pm2@latest -g
