bfcfff86868467b8ed87ca97fd583ed1aff489f9
lavegaa <cksgh405@naver.com>
2021-07-18 19:09:43 +0900

    storybook설정 및 alias를 통한 absolute path설정

--- react-app/.eslintrc.js
+++ react-app/.eslintrc.js
@@ -20,5 +20,8 @@ module.exports = {
     'react/prop-types': 0,
     'react/jsx-filename-extension': [2, { extensions: ['.js', '.jsx', '.ts', '.tsx'] }],
     'jsx-a11y/no-noninteractive-element-interactions': 0,
+    "react/jsx-props-no-spreading": "off",
+    '@typescript-eslint/no-empty-function': 'off',
+    'react/require-default-props': 'off'
   },
 };
--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import Page from '@components/Page';
+import Button from '@components/Button';
 
 function App() {
-  return <div>hello world</div>;
+  return <Button primary={false} backgroundColor="white" size="small" label="hello" />;
 }
 
 export default App;
