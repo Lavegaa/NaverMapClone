3dff374b9266565b979649ce12a284eab422a26e
lavegaa <cksgh405@naver.com>
2021-07-30 17:10:48 +0900

    lavegaa: search display추가

--- server/src/routes/search.ts
+++ server/src/routes/search.ts
@@ -1,5 +1,5 @@
 var moment = require('moment') // require
-var request = require('request');
+var request = require('request')
 
 const searchSchema = {
   response: {
@@ -16,8 +16,7 @@ const searchSchema = {
           'address',
           'roadAddress',
           'mapx',
-          'mapy'
-
+          'mapy',
         ],
         properties: {
           title: { type: 'string' },
@@ -25,45 +24,48 @@ const searchSchema = {
           category: { type: 'string' },
           description: { type: 'string' },
           telephone: { type: 'string' },
-          address:{ type: 'string' },
-          roadAddress:{ type: 'string' },
-          mapx:{ type: 'string' },
-          mapy:{ type: 'string' },
+          address: { type: 'string' },
+          roadAddress: { type: 'string' },
+          mapx: { type: 'string' },
+          mapy: { type: 'string' },
         },
       },
     },
   },
-  }
+}
 
 async function routes(fastify: any, options: any) {
-    fastify.get(
-        `/search`,
-        { schema: searchSchema },
-        async function (req: any, reply: any) {
-            try {
-              const { searchword } = req.query
-              await request.get({
-                  headers:{
-                    'content-type':'application/json',
-                    'X-Naver-Client-Id': process.env.NAVER_CLIENT_ID,
-                    'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET
-                  },
-                  url:'https://openapi.naver.com/v1/search/local.json',
-                  json:true,
-                  qs:{
-                    query:searchword
-                  }
-              },(error:any, response:any, body:any)=>{
-                if(!error){
-                  reply.send(body.items);
-                }
-              })
-            } catch (err) {
-                throw new Error(err)
+  fastify.get(
+    `/search`,
+    { schema: searchSchema },
+    async function (req: any, reply: any) {
+      try {
+        const { searchword } = req.query
+        await request.get(
+          {
+            headers: {
+              'content-type': 'application/json',
+              'X-Naver-Client-Id': process.env.NAVER_CLIENT_ID,
+              'X-Naver-Client-Secret': process.env.NAVER_CLIENT_SECRET,
+            },
+            url: 'https://openapi.naver.com/v1/search/local.json',
+            json: true,
+            qs: {
+              query: searchword,
+              display: 5,
+            },
+          },
+          (error: any, response: any, body: any) => {
+            if (!error) {
+              reply.send(body.items)
             }
-        }
-    )
-
+          }
+        )
+      } catch (err) {
+        throw new Error(err)
+      }
+    }
+  )
 }
 
 export default routes
